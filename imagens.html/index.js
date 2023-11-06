87dbfab36650378a109e9d39daa8c6568a5bc4a6
Kristiany_santos <142239011+kris0119@users.noreply.github.com>
2023-11-06 16:46:23 -0400

    Create index.js
    Javascript

---
+++ imagens.html/index.js
@@ -0,0 +1,21 @@
+ const numero = 10;
+ const texto = "10";
+ // Com dois sinais de igual (==), faz comparação de igualdade solta
+ if(numero== texto){
+ console.log("Usando ==: o número é igual ao texto(considerando igualdade solta).");
+ }else { 
+console.log("Usando ==: o número não é igual ao texto (considerando igualdade solta).");
+ }
+ //Com três sinais de igual (===), faz uma comparação de igualdade estrita
+ if(numero=== texto){
+ console.log("Usando ===: o número é igual ao texto(considerando igualdade estrita).");
+ }else{
+ console.log("Usando ===: o número não é igual ao texto(considerando igualdade estrita).");
+ }
+
+ var k = 40;
+ console.log(k*1.8+32,"graus Fahrenheit");
+
+ var c = 10;
+ var m = 12;
+ console.log(c*m,"area do retangulo");
